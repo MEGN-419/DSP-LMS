b04aebc80abbb7eb08d7d966ef532f92168738b2
hp <marwanehab419@gmail.com>
2025-11-21 21:07:03 +0200

    Refactor scripts package by replacing `dataStorage` interface with `infoClass`, expand `client` class with fields, and introduce `BTS` node structure in `dataHandler`.

--- src/handling/dataHandler.java
+++ src/handling/dataHandler.java
@@ -1,12 +1,20 @@
 package handling;
 
+import java.util.HashMap;
+
 public class dataHandler {
     public static int bookCount;
     public static int clientCount;
     public static boolean debug;
     public class storage{
-        public class BST{
-
+        node.BTS bookT = new node.BTS();
+        //TODO create genre and pubYear maps , continue bts
+        public class node{
+            int data;
+            node left , right;
+            public static class BTS{
+                node root;
+            }
         }
     }
 }
--- src/obj/client.java
+++ src/obj/client.java
@@ -1,4 +1,12 @@
 package obj;
 
+import java.util.ArrayList;
+
 public class client {
+    int memberId;
+    String name;
+    String email;
+    int memDate;
+    ArrayList<book> bbs = new ArrayList<>();
+
 }
