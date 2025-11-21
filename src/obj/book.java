002b43012c35e8faa210e2a58bfa34101bd0f357
hp <marwanehab419@gmail.com>
2025-11-21 20:35:28 +0200

    Implement `book` class with fields, constructor, and methods for updating and retrieving information.

--- src/obj/book.java
+++ src/obj/book.java
@@ -1,4 +1,57 @@
 package obj;
-
+import handling.dataHandler;
 public class book {
+    int bookID;
+    String title;
+    String author;
+    String genre;
+    int pubYear;
+    boolean stat;
+    public book(int bookID, String title , String author , String genre , int pubYear){
+        this.bookID=bookID;
+        this.title=title;
+        this.author=author;
+        this.genre=genre;
+        this.pubYear=pubYear;
+        stat = true;
+        dataHandler.bookCount++;
+        if(dataHandler.debug){
+            System.out.println("created book with data :\nbookID : "+bookID+"\ntitle : "+title+"\n genre : "+genre+"\npublication year : "+pubYear);
+        }
+    }
+    public void updateInfo(String option , String data){
+        switch(option){
+            case "bookID":
+                bookID = Integer.parseInt(data);
+            break;
+            case"title":
+                title = data;
+            break;
+            case"author":
+                author = data;
+            break;
+            case "genre":
+                genre = data;
+            break;
+            case"pubYear":
+                pubYear = Integer.parseInt(data);
+            break;
+            default:
+                if(dataHandler.debug) System.out.println("wrong option entered");
+            break;
+        }
+    }
+    public String getInfo(String option){
+        return switch (option) {
+            case "bookID" -> String.valueOf(bookID);
+            case "title" -> title;
+            case "author" -> author;
+            case "genre" -> genre;
+            case "pubYear" -> String.valueOf(pubYear);
+            default -> {
+                if (dataHandler.debug) System.out.println("wrong option entered");
+                yield null;
+            }
+        };
+    }
 }
