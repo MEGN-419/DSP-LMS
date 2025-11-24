a9ea9ddae5b6276eeae385138b91c68ce6e110e4
hp <marwanehab419@gmail.com>
2025-11-24 21:23:51 +0200

    Add `generator` class with methods for generating `book` and `client` instances using `dataHandler`

--- src/scrpts/generator.java
+++ src/scrpts/generator.java
@@ -1,4 +1,27 @@
 package scrpts;
 
+import handling.dataHandler;
+import obj.book;
+import obj.client;
+
+import java.util.Random;
+
 public class generator {
+    Random rand = new Random();
+    dataHandler dataHandler ;
+    public generator(dataHandler dataHandler){
+        this.dataHandler=dataHandler;
+    }
+    public book bookGen(){
+        return new book(1000+ dataHandler.bookCount,infoClass.bookTitles[rand.nextInt(infoClass.bookTitles.length)],
+                infoClass.humanNames[rand.nextInt(infoClass.humanNames.length)],infoClass.bookGenres[rand.nextInt(infoClass.bookGenres.length)],
+                rand.nextInt(1970,2025));
+    }
+    public client clientGen(){
+        return new client(1000+ dataHandler.clientCount,
+                infoClass.humanNames[rand.nextInt(infoClass.humanNames.length)],
+                infoClass.humanNames[rand.nextInt(infoClass.humanNames.length)]+infoClass.humanNames[rand.nextInt(infoClass.humanNames.length)]+rand.nextInt(1,999)+"@gmail.com",
+                (rand.nextInt(10,25)*10000)+(rand.nextInt(1,30)*100)+ rand.nextInt(1,30)
+                );
+    }
 }
